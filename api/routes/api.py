b1ee5da00d0459e5ee1dbfbb26492bbb65eb3353
Reality <71893501+Reality281@users.noreply.github.com>
2023-05-31 14:00:14 +0530

    *

--- api/routes/api.py
+++ api/routes/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
-BPObj = Blueprint('api', __name__)
+apiBP = Blueprint('api', __name__)
 
-@BPObj.route('/<name>')# Route to api that says hello to the user
+@apiBP.route('/<name>')# Route to api that says hello to the user
 def sayHello(name):
     return f'Hello {name}!'
