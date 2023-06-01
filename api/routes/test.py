7730c6f1c66f1548f4060ec3c5465d354b9b26c0
Reality <71893501+Reality281@users.noreply.github.com>
2023-06-01 15:20:38 +0530

    + dynamic routing test

--- api/routes/test.py
+++ api/routes/test.py
@@ -5,25 +5,3 @@ testBP = Blueprint('test', __name__)
 @testBP.route('/')
 def dynamicIndex():
     return render_template('testIndex.html')
-
-@testBP.route('/page1')
-def dynamicPage1():
-    return render_template('testPage1.html')
-
-@testBP.route('/page2')
-def dynamicPage2():
-    return render_template('testPage2.html')
-
-
-
-
-@testBP.route('/2/')
-def dynamicIndex2():
-    return render_template('testIndex2.html')
-
-@testBP.route('/update', methods=['POST'])
-def dynamicUpdate():
-    # Process the AJAX request and return updated content
-    # You can fetch data from a database or perform other operations here
-    data = {'message': 'New content'}
-    return jsonify(data)
