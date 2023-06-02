46e003cbbdfdcdd2993e6c4d6bd6d47875dbb1b7
Reality <71893501+Reality281@users.noreply.github.com>
2023-06-02 21:45:02 +0530

    *

--- api/server.py
+++ api/server.py
@@ -91,6 +91,31 @@ def getVideoInfo(videoID):
 		abort(500, f'An error occured while extracting information from `{YTVideoURL}`')
 
 
+@app.route('/video/debug/<videoID>/')# Route to display Information of the Video
+def getVideoInfoDebug(videoID):
+	YTVideoURL = getVideoLink(videoID)# Getting the yt video link using the id of the video
+	try:
+		try:
+			yt = YouTube(YTVideoURL)# Getting a YouTube object using the yt video link
+		except VideoUnavailable:
+			pass
+		views = convertViews(yt.views)# Getting the number of views the video has and converting them to more readable format
+		length = convertTime(yt.length)# Getting the duration of the video and converting them to more readable format
+		return render_template(# Rendering videoInfo.html
+		                        'videoInfoDebug.html',
+				                yt=yt,
+		                        views=views,
+		                        length=length,
+		                        websiteTitle=websiteTitle,
+		                        videoID=yt.video_id,
+		                        YTVideoURL=YTVideoURL,
+		                        videoStreams=yt.streams.filter(progressive=True).order_by('resolution'),# Getting streams with both audio and video objects and rearranging them according resolution
+		                        audioStreams=yt.streams.filter(only_audio=True))# Getting streams with only audio objects
+	except Exception as e:
+		#return showError(videoURL=YTVideoURL, errURL='/video', err=e)# Displaying error if any error occurs
+		abort(500, f'An error occured while extracting information from `{YTVideoURL}`')
+
+
 @app.route('/download/', methods=['POST'])# Route to redirect the user to the download page of the video
 def redirectToVideoDownloadPage():
 	if request.form['video_url']:
