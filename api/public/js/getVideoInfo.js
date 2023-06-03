a0bb78a516e049949b41af48afd83b902d2a5177
Reality <71893501+Reality281@users.noreply.github.com>
2023-06-03 21:07:59 +0530

    + dynamic routing debug

---
+++ api/public/js/getVideoInfo.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#getInfoBtn').click(function() {
+		$.ajax({
+			url: '/api/getVideoInfo',
+			type: 'GET',
+			contentType: 'application/json',
+			data: {
+				video_url: $('#video_url').value()
+			},
+			success: function(response) {
+				$('#getInfoBtn').text(response.videoID)
+			}
+		})
+	})
+})
--- api/routes/api.py
+++ api/routes/api.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from ..utils.videoFuncs import getVideoID, getVideoLink
 
 apiBP = Blueprint('api', __name__)
 
-@apiBP.route('/<name>')# Route to api that says hello to the user
+@apiBP.route('/<name>/')# Route to api that says hello to the user
 def sayHello(name):
     return f'Hello {name}!'
+
+
+@apiBP.route('/getVideoInfo/', methods=['GET'])
+def apiGetVideoInfo():
+    if request.is_json:
+        videoID = getVideoID(request.args.get('video_url'))
+        return jsonify({'videoID': videoID})
