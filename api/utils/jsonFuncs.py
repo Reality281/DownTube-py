3cddc62c1e42e9c7109b9579ef86644813bcda8a
Reality <71893501+Reality281@users.noreply.github.com>
2023-06-08 14:36:30 +0530

    *

--- api/utils/jsonFuncs.py
+++ api/utils/jsonFuncs.py
@@ -1,6 +1,9 @@
 import json
 
 def loadData(path):
-    with open(path) as file:
-        data = json.load(file)
-        return data
+    try:
+        with open(path) as file:
+            data = json.load(file)
+            return data
+    except Exception as e:
+        return f'An error occured while getting the data from "{path}"'
