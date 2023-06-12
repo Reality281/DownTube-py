b7ce8e1b39a95c2149bca1bd357493ea4e20d1b3
Reality <71893501+Reality281@users.noreply.github.com>
2023-06-12 16:30:51 +0530

    + dynamic loading

--- api/routes/api.py
+++ api/routes/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, render_template, request
+import pytube
 from ..utils.videoFuncs import getVideoID, getVideoLink
 
 apiBP = Blueprint('api', __name__)
@@ -6,3 +7,18 @@ apiBP = Blueprint('api', __name__)
 @apiBP.route('/<name>/')# Route to api that says hello to the user
 def sayHello(name):
     return f'Hello {name}!'
+
+@apiBP.route('/')
+def indexTest():
+    return render_template('indexTest.html')
+
+@apiBP.route('/get_video_info/')
+def get_video_info():
+    video_url = request.args.get('video_url')
+    try:
+        video = pytube.YouTube(video_url)
+        title = video.title
+        description = video.description
+        return render_template('videoInfoTest.html', title=title, description=description)
+    except Exception as e:
+        return str(e)
